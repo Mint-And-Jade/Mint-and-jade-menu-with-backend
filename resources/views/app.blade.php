67092f5f5cd6ae3760a984cc4c75e9305ff552b1
michael <michaeldebs16@gmail.com>
2022-08-23 12:52:43 +0300

    a

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 
-    @vite('resources/js/app.js')
+    <script src="../js/app.js"></script>
     @inertiaHead
 </head>
 <body>
