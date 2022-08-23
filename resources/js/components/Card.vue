845a204ea6a892bee5b3c38a1756d2cc951d3421
michael <michaeldebs16@gmail.com>
2022-08-23 23:54:52 +0300

    v1.1

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-[250px] h-full">
+    <div class="w-[325px] h-full">
         <div class="relative flex">
             <img class="w-10 absolute left-[-2.5rem]" src="../../../public/img/icon.svg" alt="Leaf">
             <h2 class="ml-1">{{ category.name }}</h2>
@@ -7,14 +7,16 @@
         <div class="mt-1 border-2 rounded-md p-4 border-[#59d460]">
             <div class="flex flex-col flex-wrap leading-7">
                 <div class="relative" v-for="item in items">
-                    <div v-if="item.category_id === category.id" class="flex justify-between mr-2">
+                    <div v-if="item.category_id === category.id" class="flex justify-between">
                         <!-- wrapper -->
-                        <div class="">
+                        <div class="w-[75%]">
                             <img src="../../../public/img/dot.svg" alt=""
                                 class="w-[8px] rounded-full absolute top-[.6rem]">
                             <p class="ml-5"> {{ item.name }}</p>
                         </div>
-                        {{ item.price }} LL
+                        <div class="w-[75px]">
+                            {{ item.price }} LL
+                        </div>
                     </div>
                 </div>
             </div>
