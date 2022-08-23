642d4ca2f3467d88861b4bffc57890046b087786
Mint-And-Jade <111539686+Mint-And-Jade@users.noreply.github.com>
2022-08-23 16:47:14 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ use App\Http\Controllers\CategoryController;
 |
 */
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 Route::get('/', function () {
     return Inertia::render('Home',[
         'items' => Item::all(),
