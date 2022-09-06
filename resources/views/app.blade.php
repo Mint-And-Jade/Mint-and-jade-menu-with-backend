9dce3ee8fce591ac4abf86f439c41b3bb022dd04
michael <michaeldebs16@gmail.com>
2022-09-06 16:03:24 +0300

    1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mint And Jade</title>
 
-    {{-- @vite('resources/js/app.js') --}}
-    <link rel="stylesheet" href="https://mintandjade1.herokuapp.com/build/assets/app.1720945e.css" /><script type="module" src="https://mintandjade1.herokuapp.com/build/assets/app.3323e11b.js"></script>
-    @inertiaHead
+    @vite('resources/js/app.js')
+    {{-- <link rel="stylesheet" href="https://mintandjade1.herokuapp.com/build/assets/app.1720945e.css" /><script type="module" src="https://mintandjade1.herokuapp.com/build/assets/app.3323e11b.js"></script> --}}
 </head>
 <body>
     @inertia
